c387601a6dfee2f09efd12a96e9ee5b13d310819
belal-abdelaal <belal.business.email@gmail.com>
2025-08-06 17:12:46 +0300

    refactor(Modules/User): Replaced the files into the User module properly, but needs namespace resolvements

--- Modules/User/app/Http/Controllers/UserController.php
+++ Modules/User/app/Http/Controllers/UserController.php
@@ -1,56 +1,34 @@
 <?php
 
-namespace Modules\User\Http\Controllers;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Http\Requests\UserLoginRequest;
+use App\Http\Requests\UserSignupRequest;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    protected $userService;
+
+    public function __construct(UserService $userService)
     {
-        return view('user::index');
+        $this->userService = $userService;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function create(UserSignupRequest $request)
     {
-        return view('user::create');
+        $data = $this->userService->validate($request);
+        if ($this->userService->create($data)) {
+            return response()->json(["message" => "Account created successfuly"], 201);
+        }
+        return response()->json(["message" => "Email already exists"], 422);
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request) {}
-
-    /**
-     * Show the specified resource.
-     */
-    public function show($id)
+    public function login(UserLoginRequest $request)
     {
-        return view('user::show');
+        $data = $this->userService->validate($request);
+        if ($token = $this->userService->login($data))
+            return response()->json(["token" => $token->plainTextToken]);
+        return response()->json(["message" => "Invalid email or password"]);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit($id)
-    {
-        return view('user::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, $id) {}
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy($id) {}
 }
--- Modules/User/database/factories/UserFactory.php
+++ Modules/User/database/factories/UserFactory.php
@@ -1,25 +1,20 @@
 <?php
 
-namespace Database\Factories;
+namespace Modules\User\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
     /**
-     * The current password being used by the factory.
+     * The name of the factory's corresponding model.
      */
+    protected $model = \Modules\User\Models\User::class;
     protected static ?string $password;
 
     /**
      * Define the model's default state.
-     *
-     * @return array<string, mixed>
      */
     public function definition(): array
     {
@@ -29,15 +24,5 @@ class UserFactory extends Factory
             'password' => static::$password ??= Hash::make('password'),
             'gender' => fake()->randomElement(['male', 'female']),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn(array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
